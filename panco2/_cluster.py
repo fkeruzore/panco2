Fix units bug in cluster + improve communication between panco2 and model

panco2/_cluster.py
@@ -27,30 +27,23 @@ class Cluster:
 
     """
 
-    def __init__(self, z, Y_500=None, M_500=None):
+    def __init__(self, z, M_500=None):
         """
 
         Parameters
         ----------
         z : float
             Cluster's redshift
-        Y_500 : float [arcmin2]
-            Cluster's integrated Compton parameter within R_500.
         M_500 : float [Msun]
             Cluster's mass within R_500.
-
-        Raises
-        ------
-        Exception : if neither Y_500 nor M_500 are specified.
-
         """
 
         # Cosmology related parameters
         self.cosmo = FlatLambdaCDM(70.0 * u.Unit("km s-1 Mpc-1"), 0.3)
         self.z = z
         self.E = self.cosmo.efunc(z)
-        self.dens_crit = self.cosmo.critical_density(z).to("g/cm3")
-        self.d_a = self.cosmo.angular_diameter_distance(z).to("kpc")
+        self.dens_crit = self.cosmo.critical_density(z).to("Msun kpc-3").value
+        self.d_a = self.cosmo.angular_diameter_distance(z).to("kpc").value
         h_70 = self.cosmo.h / 0.7
         self.sz_fact = (
             (const.sigma_T / (const.m_e * const.c**2))
@@ -59,53 +52,25 @@ class Cluster:
         )
 
         # Prior information on characteristic quantities
-        if (M_500 is None) and (Y_500 is None):
-            raise Exception(
-                "Need either Y_500 or M_500 to initialize "
-                + "`cluster` object"
-            )
-        if Y_500 is not None:
-            self.Y_500_arcmin2 = Y_500
-            Y_500_Mpc2 = (
-                Y_500.to("rad2") * (self.d_a.to("Mpc")) ** 2
-            ) / u.rad**2
-            self.Y_500_kpc2 = Y_500_Mpc2.to("kpc2").value
-            if M_500 is None:
-                self.M_500 = (
-                    3.0e14
-                    / h_70
-                    * (
-                        10**4.739
-                        * self.E ** (-2.0 / 3.0)
-                        * h_70**2.5
-                        * Y_500_Mpc2
-                    )
-                    ** (1.0 / 1.79)
-                )  # Msun
         self.M_500 = M_500
-        R_500 = (3 * self.M_500 / (4 * np.pi * 500 * self.dens_crit)) ** (
+        self.R_500 = (3 * self.M_500 / (4 * np.pi * 500 * self.dens_crit)) ** (
             1.0 / 3.0
         )
-        self.R_500 = R_500.to("kpc").value
-        self.theta_500 = np.arctan(R_500 / self.d_a.value) * u.rad.to("arcmin")
+        self.theta_500 = np.arctan(self.R_500 / self.d_a)
+        self.theta_500 *= u.rad.to("arcmin")
 
         # Arnaud et al. (2010) pressure profile parameters
         self.P_500 = (
             1.65e-3
-            * (self.E) ** (8.0 / 3)
-            * ((self.M_500 * h_70) / ((3.0e14) * const.M_sun))
-            ** (2.0 / 3.0 + 0.12)
+            * self.E ** (8.0 / 3)
+            * ((self.M_500 * h_70 / 3e14) ** (2.0 / 3.0 + 0.12))
             * (h_70) ** 2
-            * u.keV
-            / u.cm**3
-        ).to(
-            "keV cm-3"
-        )  # equation (13) of A10
+        ) # equation (13) of A10, slightly modified notation
 
-        self.A10_params = np.array(
+        self.A10_params = np.array( # equation (12) of A10
             [
-                8.403 * self.P_500.value,
-                self.R_500.value / 1.177,
+                8.403 * self.P_500,
+                self.R_500 / 1.177,
                 1.0510,
                 5.4905,
                 0.3081,

panco2/panco2.py
@@ -8,25 +8,19 @@ from astropy.nddata import Cutout2D
 from astropy.coordinates import SkyCoord
 import emcee
 from iminuit import Minuit
-import covar
 from scipy.interpolate import interp1d
 from scipy.ndimage import gaussian_filter
 import sys
 import os
 import shutil
 import json
+import dill
 import time
 import argparse
 from multiprocessing import Pool
 import pdb
 from _cluster import Cluster
-import _data
-import _xray
-import _probability
-import _utils
-import _model_gnfw, _model_nonparam
-import _fit_gnfw_on_non_param
-import _results
+import model
 
 
 class PressureProfileFitter:
@@ -43,18 +37,9 @@ class PressureProfileFitter:
         Index of the FITS extension in which the RMS map is stored
     z : float
         Cluster's redshift
-    Y_500 : float [arcmin2], optional
-        Integrated Compton parameter within R_500.
-        This is used to build the starting point of the MCMC.
-        Either one of Y_500 or M_500 need to be specified.
-    M_500 : float [Msun], optional
+    M_500 : float [Msun]
         A guess of the cluster's mass.
         This is used to build the starting point of the MCMC.
-        Either one of Y_500 or M_500 need to be specified.
-    beam_fwhm : float [arcsec], optional
-        The FWHM of your gaussian beam.
-        Can be set to 0 if you don't want beam confolution
-            (e.g. if the beam is already in the transfer function).
     coords_center : SkyCoord, optional
         Coordinate to consider as the center of the map
     map_size : float [arcmin], optional
@@ -67,14 +52,12 @@ class PressureProfileFitter:
         hdu_data,
         hdu_rms,
         z,
-        Y_500=None,
-        M_500=None,
-        beam_fwhm=0.0,
+        M_500,
         coords_center=None,
         map_size=None,
     ):
 
-        self.cluster = Cluster(z, Y_500=Y_500, M_500=M_500)
+        self.cluster = Cluster(z, M_500=M_500)
 
         # ===== Read and formats the data ===== #
         hdulist = fits.open(sz_map_file)
@@ -96,15 +79,15 @@ class PressureProfileFitter:
             # Ensure the number of pixels will be odd after map_sizeping
             new_npix = int(map_size * 60 / pix_size)
             if new_npix % 2 == 0.0:
-                map_size += pix_size
+                map_size += pix_size / 60.0
                 new_npix += 1
 
             map_sizeped_map = Cutout2D(
-                hdulist[hdu_data].data, coords_center, map_size, wcs=wcs
+                hdulist[hdu_data].data, coords_center, new_npix, wcs=wcs
             )
             self.sz_map = map_sizeped_map.data
             map_sizeped_rms = Cutout2D(
-                hdulist[hdu_rms].data, coords_center, map_size, wcs=wcs
+                hdulist[hdu_rms].data, coords_center, new_npix, wcs=wcs
             )
             self.sz_rms = map_sizeped_rms.data
             self.wcs = map_sizeped_map.wcs
@@ -119,65 +102,18 @@ class PressureProfileFitter:
         self.coords_center = coords_center
         self.map_size = map_size  # arcmin
 
-        # By default no transfer function convolution
-        def convolve_tf(x):
-            return x
+    # ---------------------------------------------------------------------- #
 
-        self.__convolve_tf = convolve_tf
+    @classmethod
+    def load_from_file(cls, file_name):
+        with open(file_name, "rb") as f:
+            inst = dill.load(f)
+        return inst
 
-        # Beam convolution
-        if beam_fwhm == 0.0:
+    def dump_to_file(self, file_name):
+        with open(file_name, "wb") as f:
+            f.write(dill.dumps(self))
 
-            def convolve_beam(x):
-                return x
-
-        else:
-            beam_sigma_pix = (
-                beam_fwhm / (2 * np.sqrt(2 * np.log(2))) / pix_size
-            )
-
-            def convolve_beam(x):
-                return gaussian_filter(x, beam_sigma_pix)
-
-        self.__convolve_beam = convolve_beam
-
-        """
-        model_type = model_type.lower()
-        if model_type == "gnfw":
-            self.model = _model_gnfw.ModelGNFW(self.cluster, zero_level=fit_zero)
-        elif model_type == "binned":
-            self.model = _model_nonparam.ModelNonParam(
-                self.cluster, zero_level=fit_zero
-            )
-        else:
-            raise Exception("Unrecognized `model_type`: " + model_type)
-
-        n_bins = 5  # TODO
-        self.model.init_profiles_radii(
-            reso=pix_size,
-            npix=sz_map.shape[0],
-            r_min_z=1e-3,
-            r_max_z=5 * self.cluster.R_500_kpc,
-            nbins_z=100,
-            mode="500",  # TODO
-            radius_tab=radial_bins,
-            n_bins=n_bins,
-        )
-
-        side_tf = 9 * u.arcmin  # TODO: this should NOT have to be hardcoded
-        pad_tf = int(0.5 * _utils.adim(side_tf / pix_size))
-        self.model.init_transfer_function(
-            file_tf,
-            sz_map.shape[0],
-            pad_tf,
-            pix_size.to("arcsec").value,
-            beam_fwhm / (2 * np.sqrt(2 * np.log(2))) / pix_size,
-        )
-
-        # TODO add point sources here?
-
-        self.model.init_param_indices()
-        """
 
     # ---------------------------------------------------------------------- #
 
@@ -228,8 +164,8 @@ class PressureProfileFitter:
 
             # numpy needs help for large covariace matrices
             # see https://pythonhosted.org/covar/
-            covmat, shrink = covar.cov_shrink_rblw(covmat, n_maps)
-            print(f"    Covariance shrinkage: {shrink}")
+            # covmat, shrink = covar.cov_shrink_rblw(covmat, n_maps)
+            # print(f"    Covariance shrinkage: {shrink}")
             self.inv_covmat = np.linalg.inv(covmat)
 
         elif inv_covmat_file is not None:
@@ -238,13 +174,17 @@ class PressureProfileFitter:
 
     # ---------------------------------------------------------------------- #
 
-    def add_transfer_function(self, tf_k, k, pad=60.0):
+    def add_filtering(self, beam_fwhm=0.0, tf_k=None, k=None, pad=60.0):
         """
-        Initialize a transfer function convolution in the
-        model computation.
+        Initialize convolution by a beam and
+        transfer function in the model computation.
 
         Parameters
         ----------
+        beam_fwhm : float [arcsec], optional
+            The FWHM of your gaussian beam.
+            Can be set to 0 if you don't want beam confolution
+                (e.g. if the beam is already in the transfer function).
         tf_k : array
             Filtering measurement.
         k : array [arcmin-1]
@@ -258,33 +198,39 @@ class PressureProfileFitter:
             i.e. the largest 1D mode is 1/(pixel size).
         """
 
-        # Compute the modes covered in the map
-        npix = self.sz_map.shape[0]
-        k_vec = np.fft.fftfreq(npix + 2 * pad, self.pix_size)
-        karr = np.hypot(*np.meshgrid(k_vec, k_vec))
+        # Transfer function filtering
+        if (k is not None) and (tf_k is not None):
+            # Compute the modes covered in the map
+            npix = self.sz_map.shape[0]
+            k_vec = np.fft.fftfreq(npix + 2 * pad, self.pix_size)
+            karr = np.hypot(*np.meshgrid(k_vec, k_vec))
 
-        interp = interp1d(k, tf_k, bounds_error=False, fill_value=tf_k[-1])
-        tf_arr = interp(karr)
+            interp = interp1d(k, tf_k, bounds_error=False, fill_value=tf_k[-1])
+            tf_arr = interp(karr)
 
-        self.transfer_function = {"k": karr, "tf_k": tf_arr}
-        self.pad_pix = int(pad / self.pix_size)
+            self.transfer_function = {"k": karr, "tf_k": tf_arr}
+            self.pad_pix = int(pad / self.pix_size)
 
-        def convolve_tf(in_map):
-            in_map_pad = np.pad(
-                in_map, self.pad_pix, mode="constant", constant_values=0.0
+            def convolve_tf(in_map):
+                in_map_pad = np.pad(
+                    in_map, self.pad_pix, mode="constant", constant_values=0.0
+                )
+                in_map_fourier = np.fft.fft2(in_map_pad)
+                conv_in_map = np.real(np.fft.ifft2(in_map_fourier * tf_arr))
+                return conv_in_map[pad:-pad, pad:-pad]
+
+            self.model.convolve_tf = convolve_tf
+
+        # Beam convolution
+        if beam_fwhm != 0.0:
+            beam_sigma_pix = (
+                beam_fwhm / (2 * np.sqrt(2 * np.log(2))) / self.pix_size
             )
-            in_map_fourier = np.fft.fft2(in_map_pad)
-            conv_in_map = np.real(np.fft.ifft2(in_map_fourier * tf_arr))
-            return conv_in_map[pad:-pad, pad:-pad]
 
-        self.__convolve_tf = convolve_tf
+            def convolve_beam(x):
+                return gaussian_filter(x, beam_sigma_pix)
 
-    # ---------------------------------------------------------------------- #
-    def convolve_beam(self, in_map):
-        return self.__convolve_beam(in_map)
-
-    def convolve_tf(self, in_map):
-        return self.__convolve_tf(in_map)
+            self.model.convolve_beam = convolve_beam
 
     # ---------------------------------------------------------------------- #
 
@@ -298,11 +244,14 @@ class PressureProfileFitter:
 
         self.model_type = model_type.lower()
         d_a = self.cluster.d_a
+        npix = self.sz_map.shape[0]
 
         # ===== Radii arrays ===== #
         # 1D radius in the sky plane, only half the map
         theta_x = (
-            np.arange(0, int(npix / 2) + 1) * reso_rad
+            np.arange(0, int(npix / 2) + 1)
+            * self.pix_size
+            * u.arcsec.to("rad")
         )  # angle, in radians
         r_x = d_a * np.tan(theta_x)  # distance, in kpc
 
@@ -314,7 +263,7 @@ class PressureProfileFitter:
             )
         )
 
-        self.__radii = {"r_x": r_x, "r_xy": r_xy, "theta_x": theta_x}
+        self.radii = {"r_x": r_x, "r_xy": r_xy, "theta_x": theta_x}
 
         # If gNFW, we need to define the line of sight
         if self.model_type == "gnfw":
@@ -325,11 +274,19 @@ class PressureProfileFitter:
             r_xx, r_zz = np.meshgrid(r_x, r_z)
             r_xz = np.hypot(r_xx, r_zz)
 
-            self.__radii["r_z"] = r_z
-            self.__radii["r_zz"] = r_zz
-            self.__radii["r_xz"] = r_xz
+            self.radii["r_z"] = r_z
+            self.radii["r_zz"] = r_zz
+            self.radii["r_xz"] = r_xz
+
+        self.model = model.ModelBinned(
+            r_bins, self.radii, zero_level=zero_level
+        )
 
     # ---------------------------------------------------------------------- #
+
+    def run_mcmc(self, max_steps):
+        pass
+
     # ---------------------------------------------------------------------- #
     # ---------------------------------------------------------------------- #
     # ---------------------------------------------------------------------- #